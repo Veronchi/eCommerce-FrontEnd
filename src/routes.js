26f8de673a02347028748e9528ec9bebcff70fa4
Veronika <zhigunova1995@gmail.com>
2022-07-19 17:20:40 +0300

    add routes

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import {
+  ADMIN_ROUTE,
+  BASKET_ROUTE,
+  LOGIN_ROUTE,
+  PRODUCT_ROUTE,
+  REGISTRATION_ROUTE,
+  SHOP_ROUTE,
+} from "./utils/consts";
+import Shop from './pages/Shop'
+import Admin from './pages/Admin'
+import Basket from './pages/Basket'
+import Auth from './pages/Auth'
+import ProductPage from './pages/ProductPage'
+
+
+export const authRoutes = [
+  {
+    path: ADMIN_ROUTE,
+    Component: Admin,
+  },
+  {
+    path: BASKET_ROUTE,
+    Component: Basket,
+  },
+];
+
+export const publicRoutes = [
+  {
+    path: SHOP_ROUTE,
+    Component: Shop,
+  },
+  {
+    path: LOGIN_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: REGISTRATION_ROUTE,
+    Component: Auth,
+  },
+  {
+    path: PRODUCT_ROUTE + "/:id",
+    Component: ProductPage,
+  },
+];
