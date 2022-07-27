dae3c265bfaa73284353dc57eac051829b43ded8
Veronika <zhigunova1995@gmail.com>
2022-07-27 11:55:41 +0300

    implement  bar of categories

---
+++ src/components/CategoryBar/CategoryBar.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { Context } from "../..";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./style.css"
+
+const CategoryBar = observer(() => {
+  const { category } = useContext(Context);
+
+  return (
+    <ListGroup>
+      {category.categories.map((item) => {
+        return (
+          <ListGroup.Item
+            className="list-item"
+            active={item.id === category.selectedCategory.id}
+            onClick={() => category.setSelectedCategory(item)}
+            key={item.id}
+          >
+            {item.name}
+          </ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
+});
+
+export default CategoryBar;
--- src/store/CategoryStore.js
+++ src/store/CategoryStore.js
@@ -5,7 +5,10 @@ export default class CategoryStore {
     this._categories = [
       { id: 1, name: "Холодильник" },
       { id: 2, name: "Телефон" },
+      { id: 3, name: "Кофеварки" },
+      { id: 4, name: "Кофемашины" },
     ];
+    this._selectedCategory = {};
     makeAutoObservable(this);
   }
 
@@ -13,7 +16,15 @@ export default class CategoryStore {
     this._isAuth = categories;
   }
 
+  setSelectedCategory(category) {
+    this._selectedCategory = category;
+  }
+
   get categories() {
     return this._categories;
+  }
+
+  get selectedCategory() {
+    return this._selectedCategory;
   }
 }
