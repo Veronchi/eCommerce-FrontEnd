459180d9a6c000fb38cf599f4a1b52f72f34d000
Veronika <zhigunova1995@gmail.com>
2022-07-28 13:13:00 +0300

    change element type

--- src/components/CategoryBar/CategoryBar.js
+++ src/components/CategoryBar/CategoryBar.js
@@ -2,16 +2,17 @@ import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import { Context } from "../..";
 import ListGroup from "react-bootstrap/ListGroup";
-import "./style.css"
+import "./style.css";
 
 const CategoryBar = observer(() => {
   const { category } = useContext(Context);
 
   return (
-    <ListGroup>
+    <ListGroup as="ul">
       {category.categories.map((item) => {
         return (
           <ListGroup.Item
+            as="li"
             className="list-item"
             active={item.id === category.selectedCategory.id}
             onClick={() => category.setSelectedCategory(item)}
