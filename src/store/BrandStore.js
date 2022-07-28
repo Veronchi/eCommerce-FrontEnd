736c8ebbec4308571f15e55426b1da709727189b
Veronika <zhigunova1995@gmail.com>
2022-07-28 16:35:47 +0300

    add selecting brand

--- src/store/BrandStore.js
+++ src/store/BrandStore.js
@@ -5,7 +5,11 @@ export default class BrandStore {
     this._brands = [
       { id: 1, name: "Sumsung" },
       { id: 2, name: "Apple" },
+      { id: 3, name: "Intel" },
+      { id: 4, name: "Honor" },
+      { id: 5, name: "Nivona" },
     ];
+    this._selectedBrand = {};
     makeAutoObservable(this);
   }
 
@@ -13,7 +17,15 @@ export default class BrandStore {
     this._isAuth = brands;
   }
 
+  setSelectedBrand(brand) {
+    this._selectedBrand = brand;
+  }
+
   get brands() {
     return this._brands;
+  }
+
+  get selectedBrand() {
+    return this._selectedBrand;
   }
 }
