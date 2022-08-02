2872710f4829d207aad71a3a3b79f64b187880a5
Veronika <zhigunova1995@gmail.com>
2022-08-02 16:32:44 +0300

    change path

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import Shop from './pages/Shop/Shop'
 import Admin from './pages/Admin'
 import Basket from './pages/Basket'
 import Auth from './pages/Auth/Auth'
-import ProductPage from './pages/ProductPage'
+import ProductPage from './pages/ProductPage/ProductPage'
 
 
 export const authRoutes = [
