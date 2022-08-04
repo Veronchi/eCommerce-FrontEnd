11ab0472b506706020cde8373c45f162e139d23f
Veronika <zhigunova1995@gmail.com>
2022-08-04 16:12:55 +0300

    refactor product item component

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Image } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import star from "../../assets/star.svg";
 import { PRODUCT_ROUTE } from "../../utils/consts";
@@ -10,19 +10,21 @@ const ProductItem = ({ product }) => {
   const navigate = useNavigate();
 
   return (
-    <Col md={3} onClick={() => navigate(PRODUCT_ROUTE + "/" + product.id)}>
-      <Card className="product-card" border={"light"}>
-        <Image className="product-img" src={product.img} />
-        <div className="card-info d-flex justify-content-between align-items-center">
-          <div>Samsung...</div>
-          <div className="d-flex align-items-center">
-            <div>{product.rating}</div>
-            <Image src={star} />
-          </div>
+    <Card
+      className="product-card"
+      border={"light"}
+      onClick={() => navigate(PRODUCT_ROUTE + "/" + product.id)}
+    >
+      <Image className="product-img" src={product.img} />
+      <div className="card-info d-flex justify-content-between align-items-center">
+        <div>Samsung...</div>
+        <div className="d-flex align-items-center">
+          <div>{product.rating}</div>
+          <Image src={star} />
         </div>
-        <div>{product.name}</div>
-      </Card>
-    </Col>
+      </div>
+      <div>{product.name}</div>
+    </Card>
   );
 };
 
