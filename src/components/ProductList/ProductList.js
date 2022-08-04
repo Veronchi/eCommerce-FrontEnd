e3c961706fc7a5903563bc6f28d96b760797dba3
Veronika <zhigunova1995@gmail.com>
2022-08-04 16:13:35 +0300

    refactor product list component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,16 +2,16 @@ import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
 import { Context } from "../..";
 import { Row } from "react-bootstrap";
-import "./style.css"
+import "./style.css";
 import ProductItem from "../ProductItem/ProductItem";
 
 const ProductList = observer(() => {
-  const {product} = useContext(Context);
-  
+  const { product } = useContext(Context);
+
   return (
-    <Row className="d-flex">
+    <Row className="d-flex justify-content-between">
       {product.products.map((item) => {
-        return <ProductItem key={item.id} product={item}/>
+        return <ProductItem key={item.id} product={item} />;
       })}
     </Row>
   );
