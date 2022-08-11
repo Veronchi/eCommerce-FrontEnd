dcd0232efe45a240b837c85193d0942d07fc654a
Veronika <zhigunova1995@gmail.com>
2022-08-11 13:41:45 +0300

    implement a modal window for the category

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
-import "./style.css"
+import CreateCategoryModal from "../../components/modals/CreateCategoryModal/CreateCategoryModal";
+import "./style.css";
 
 const Admin = () => {
-  console.log(111)
+  console.log(111);
   return (
     <Container className="admin-container d-flex flex-column">
-      <Button className="add-btn" variant={"outline-dark"}>Add cotegory</Button>
-      <Button className="add-btn" variant={"outline-dark"}>Add brand</Button>
-      <Button className="add-btn" variant={"outline-dark"}>Add product</Button>
+      <Button className="add-btn" variant={"outline-dark"}>
+        Add cotegory
+      </Button>
+      <Button className="add-btn" variant={"outline-dark"}>
+        Add brand
+      </Button>
+      <Button className="add-btn" variant={"outline-dark"}>
+        Add product
+      </Button>
+      <CreateCategoryModal show={false}/>
     </Container>
   );
 };
