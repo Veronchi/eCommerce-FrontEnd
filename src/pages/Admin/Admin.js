234f7fbda2f9c8c7627fac7d1f89e127081edc9d
Veronika <zhigunova1995@gmail.com>
2022-08-12 18:33:33 +0300

    add modal windows for admin page

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container } from "react-bootstrap";
+import CreateBrandModal from "../../components/modals/CreateBrandModal/CreateBrandModal";
 import CreateCategoryModal from "../../components/modals/CreateCategoryModal/CreateCategoryModal";
+import CreateProductModal from "../../components/modals/CreateProductModal/CreateProductModal";
 import "./style.css";
 
 const Admin = () => {
-  console.log(111);
+  const [isBrandVisible, setIsBrandVisible] = useState(false);
+  const [isCategoryVisible, setIsCategoryVisible] = useState(false);
+  const [isProductVisible, setIsProductVisible] = useState(false);
+
   return (
     <Container className="admin-container d-flex flex-column">
-      <Button className="add-btn" variant={"outline-dark"}>
+      <Button
+        className="add-btn"
+        variant={"outline-dark"}
+        onClick={() => setIsCategoryVisible(true)}
+      >
         Add cotegory
       </Button>
-      <Button className="add-btn" variant={"outline-dark"}>
+      <Button
+        className="add-btn"
+        variant={"outline-dark"}
+        onClick={() => setIsBrandVisible(true)}
+      >
         Add brand
       </Button>
-      <Button className="add-btn" variant={"outline-dark"}>
+      <Button
+        className="add-btn"
+        variant={"outline-dark"}
+        onClick={() => setIsProductVisible(true)}
+      >
         Add product
       </Button>
-      <CreateCategoryModal show={false}/>
+      <CreateCategoryModal
+        show={isCategoryVisible}
+        onHide={() => setIsCategoryVisible(false)}
+      />
+      <CreateBrandModal
+        show={isBrandVisible}
+        onHide={() => setIsBrandVisible(false)}
+      />
+      <CreateProductModal
+        show={isProductVisible}
+        onHide={() => setIsProductVisible(false)}
+      />
     </Container>
   );
 };
