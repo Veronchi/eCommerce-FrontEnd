f10d03302829fa3a726d4a5fc46bf77270134d3e
Veronika <zhigunova1995@gmail.com>
2022-08-26 13:35:59 +0300

    implement sending data for authorization

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,12 +1,37 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
 import { Button, Card, Container, Form } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { registration, signIn } from "../../http/userAPI";
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../../utils/consts";
+import { useStore } from "../../hook/useStore";
 import "./style.css";
 
-const Auth = () => {
+const Auth = observer(() => {
+  const { user } = useStore();
   const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+  const navigate = useNavigate();
   const isLogin = location.pathname === LOGIN_ROUTE;
+  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const click = async () => {
+    try {
+      let data;
+      if (isLogin) {
+        data = await signIn(login, password);
+      } else {
+        data = await registration(email, login, password);
+      }
+      user.setUser(data);
+      user.setIsAuth(true);
+      navigate(from, { replace: true });
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
 
   return (
     <Container className="auth-container d-flex justify-content-center">
@@ -14,10 +39,26 @@ const Auth = () => {
         <h2 className="card-title">{isLogin ? "Sign in" : "Create account"}</h2>
         <Form className="d-flex flex-column">
           {isLogin ? null : (
-            <Form.Control className="form-input" placeholder="Email" />
+            <Form.Control
+              className="form-input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           )}
-          <Form.Control className="form-input" placeholder="Login" />
-          <Form.Control className="form-input" placeholder="Password" />
+          <Form.Control
+            className="form-input"
+            placeholder="Login"
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <Form.Control
+            className="form-input"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <div className="btn-container">
             {isLogin ? (
               <Link className="form-link" to={REGISTRATION_ROUTE}>
@@ -29,7 +70,11 @@ const Auth = () => {
               </Link>
             )}
 
-            <Button className="form-btn" variant={"outline-success"}>
+            <Button
+              className="form-btn"
+              onClick={click}
+              variant={"outline-success"}
+            >
               Continue
             </Button>
           </div>
@@ -37,6 +82,6 @@ const Auth = () => {
       </Card>
     </Container>
   );
-};
+});
 
 export default Auth;
