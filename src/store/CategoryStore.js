06602f02c3909f7d374876d9474d9893ce5357c8
Veronika <zhigunova1995@gmail.com>
2022-08-30 16:23:37 +0300

    delete mock data and change variable

--- src/store/CategoryStore.js
+++ src/store/CategoryStore.js
@@ -2,18 +2,13 @@ import { makeAutoObservable } from "mobx";
 
 export default class CategoryStore {
   constructor() {
-    this._categories = [
-      { id: 1, name: "Холодильник" },
-      { id: 2, name: "Телефон" },
-      { id: 3, name: "Кофеварки" },
-      { id: 4, name: "Кофемашины" },
-    ];
+    this._categories = [];
     this._selectedCategory = {};
     makeAutoObservable(this);
   }
 
   setCategories(categories) {
-    this._isAuth = categories;
+    this._categories = categories;
   }
 
   setSelectedCategory(category) {
