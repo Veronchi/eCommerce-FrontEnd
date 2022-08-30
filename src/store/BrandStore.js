acc8ad0fdc416169bde80afd01f23aececca49f5
Veronika <zhigunova1995@gmail.com>
2022-08-30 16:23:18 +0300

    delete mock data and change a variable

--- src/store/BrandStore.js
+++ src/store/BrandStore.js
@@ -2,19 +2,13 @@ import { makeAutoObservable } from "mobx";
 
 export default class BrandStore {
   constructor() {
-    this._brands = [
-      { id: 1, name: "Sumsung" },
-      { id: 2, name: "Apple" },
-      { id: 3, name: "Intel" },
-      { id: 4, name: "Honor" },
-      { id: 5, name: "Nivona" },
-    ];
+    this._brands = [];
     this._selectedBrand = {};
     makeAutoObservable(this);
   }
 
   setBrands(brands) {
-    this._isAuth = brands;
+    this._brands = brands;
   }
 
   setSelectedBrand(brand) {
