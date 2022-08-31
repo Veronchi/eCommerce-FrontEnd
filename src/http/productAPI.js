5977c3387b30ed92af3cc05f3656b9e7eb51b208
Veronika <zhigunova1995@gmail.com>
2022-08-31 15:36:45 +0300

    add api for product

---
+++ src/http/productAPI.js
@@ -0,0 +1,45 @@
+import { $host, $authHost } from ".";
+
+export const createCategory = async (category) => {
+  const { data } = await $authHost.post("api/category", category);
+
+  return data;
+};
+
+export const fetchCategory = async () => {
+  const { data } = await $host.get("api/category");
+
+  return data;
+};
+
+export const createBrand = async (brand) => {
+  const { data } = await $authHost.post("api/brand", brand);
+
+  return data;
+};
+
+export const fetchBrand = async () => {
+  const { data } = await $host.get("api/brand");
+
+  return data;
+};
+
+export const createProduct = async (product) => {
+  const { data } = await $authHost.post("api/product", product);
+
+  return data;
+};
+
+export const fetchProducts = async () => {
+  const { data } = await $host.get("api/product");
+  return data.rows.map((item) => ({
+    ...item,
+    img: `${process.env.REACT_APP_API_URL}/${item.img}`,
+  }));
+};
+
+export const fetchOneProduct = async (id) => {
+  const { data } = await $host.get("api/product/" + id);
+
+  return data;
+};
